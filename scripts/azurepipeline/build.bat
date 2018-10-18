2b9e292eac8a21b3fc78200644645d3ab9cdabc7
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2018-10-18 21:21:09 +0200

    More testing

--- scripts/azurepipeline/build.bat
+++ scripts/azurepipeline/build.bat
@@ -1,10 +1,11 @@
 :: Clone WindowsCompile
+cd ..
 git clone https://github.com/acasta69/WindowsCompile.git
 git clone https://github.com/acasta69/WindowsCompileDeps.git
 
 :: Clone LuxCore (this is a bit a waste but WindowsCompile procedure
 :: doesn't work with symbolic links)
-git clone https://github.com/acasta69/Luxcore.git
+::git clone https://github.com/acasta69/Luxcore.git
 dir
 cd WindowsCompile
 ./cmake-build-x64.bat
