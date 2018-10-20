d03bb070ce9a71bf500727c7dd9e4c68c9bd9a90
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2018-10-20 18:33:07 +0200

    More testing

--- scripts/azurepipeline/build.bat
+++ scripts/azurepipeline/build.bat
@@ -1,25 +1,16 @@
-:: Clone WindowsCompile
+:: Setting up dependencies
 cd ..
 git lfs install
 git clone --branch master https://github.com/acasta69/WindowsCompile .\WindowsCompile
 git clone --branch master https://github.com/acasta69/WindowsCompileDeps .\WindowsCompileDeps
-::git -c filter.lfs.smudge= -c filter.lfs.required=false -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks clone --branch master https://github.com/acasta69/WindowsCompileDeps .\WindowsCompileDeps
-cd WindowsCompileDeps
-git lfs pull
-cd ..
 
 mklink /J Luxcore .\s
 
-dir
-dir Luxcore
-dir /S WindowsCompileDeps\x64
-:: Clone LuxCore (this is a bit a waste but WindowsCompile procedure
-:: doesn't work with symbolic links)
-::git clone https://github.com/acasta69/Luxcore.git
+"C:\Program Files\7-Zip\7z.exe"
+"C:\Program Files (x86)\7-Zip\7z.exe"
+
 copy .\Luxcore\cmake-build-x64.bat .\WindowsCompile\cmake-build-x64.cmd
 cd WindowsCompile
-::cmd /k ""C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat""
 call .\cmake-build-x64.cmd /no-ocl
-exit
 ::mv target-64-sse2/LuxCore.tar.bz2 target-64-sse2/luxcorerender-latest-linux64.tar.bz2
 cd ..
