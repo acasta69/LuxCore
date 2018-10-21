e7f4c8f5dabe7e61499f9c06a38b4ae02000f577
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2018-10-21 18:05:53 +0200

    More testing

---
+++ create-standalone.bat
@@ -0,0 +1,42 @@
+SET DIR=luxcorerender
+
+:: Remove folder if it already exists
+rd /s /q %DIR%
+
+:: Create new folder for the binaries
+md %DIR%
+
+:: Create pyluxcoretools.zip (cmake tar doesn't work on Windows)
+.\support\bin\7za.exe a ..\LuxCore\lib\pyluxcoretools.zip ..\LuxCore\src\pyluxcoretools\pyluxcoretools
+
+:: Pack pyluxcoretools
+cd ..\LuxCore
+pyinstaller samples\pyluxcoretool\pyluxcoretool.win.spec
+cd ..\WindowsCompile
+
+:: Copy pyluxcoretools binaries
+xcopy ..\LuxCore\dist\pyluxcoretool.exe %DIR%
+
+:: Copy binaries
+xcopy .\Build_CMake\LuxCore\bin\Release\luxcoreui.exe %DIR%
+xcopy .\Build_CMake\LuxCore\lib\Release\pyluxcore.pyd %DIR%
+xcopy ..\LuxCore\lib\pyluxcoretools.zip %DIR%
+
+:: Copy DLLs from WindowsCompileDeps (assuming it is in same folder as WindowsCompile)
+xcopy ..\WindowsCompileDeps\x64\Release\lib\embree3.dll %DIR%
+xcopy ..\WindowsCompileDeps\x64\Release\lib\tbb.dll %DIR%
+xcopy ..\WindowsCompileDeps\x64\Release\lib\tbbmalloc.dll %DIR%
+xcopy ..\WindowsCompileDeps\x64\Release\lib\OpenImageIO.dll %DIR%
+
+:: Copy addition files from LuxCore (assuming it is in same folder as WindowsCompile)
+xcopy ..\LuxCore\README.md %DIR%
+xcopy ..\LuxCore\COPYING.txt %DIR%
+xcopy ..\LuxCore\AUTHORS.txt %DIR%
+md %DIR%\scenes
+md %DIR%\scenes\cornell
+xcopy ..\LuxCore\scenes\cornell\cornell.cfg %DIR%\scenes\cornell
+xcopy ..\LuxCore\scenes\cornell\cornell.scn %DIR%\scenes\cornell
+xcopy ..\LuxCore\scenes\cornell\Khaki.ply %DIR%\scenes\cornell
+xcopy ..\LuxCore\scenes\cornell\HalveRed.ply %DIR%\scenes\cornell
+xcopy ..\LuxCore\scenes\cornell\DarkGreen.ply %DIR%\scenes\cornell
+xcopy ..\LuxCore\scenes\cornell\Grey.ply %DIR%\scenes\cornell
--- scripts/azurepipeline/build.bat
+++ scripts/azurepipeline/build.bat
@@ -3,15 +3,12 @@ cd ..
 git lfs install
 git clone --branch master https://github.com/acasta69/WindowsCompile .\WindowsCompile
 git clone --branch master https://github.com/acasta69/WindowsCompileDeps .\WindowsCompileDeps
-dir
+dir C:\
+dir "C:\Program Files"
 mklink /J Luxcore .\s
 
-echo C:\Program Files\7-Zip\7z.exe
-"C:\Program Files\7-Zip\7z.exe"
-echo C:\Program Files (x86)\7-Zip\7z.exe
-"C:\Program Files (x86)\7-Zip\7z.exe"
-
 copy .\Luxcore\cmake-build-x64.bat .\WindowsCompile\cmake-build-x64.cmd
+copy .\Luxcore\create-standalone.bat .\WindowsCompile\create-standalone.cmd
 cd WindowsCompile
 call .\cmake-build-x64.cmd /no-ocl
 ::mv target-64-sse2/LuxCore.tar.bz2 target-64-sse2/luxcorerender-latest-linux64.tar.bz2
