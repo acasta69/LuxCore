7634beacd9d3c3d270e61a87c69015f293f27c66
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2018-10-28 13:26:07 +0100

    More testing

--- scripts/azurepipeline/test2.bat
+++ scripts/azurepipeline/test2.bat
@@ -1,2 +1,6 @@
-set
-find /I "bat" *.yml
+cmake --version
+cmake --version | findstr /C:"cmake version"
+for /F "tokens=3" %%G in ('cmd /c "%CMAKE%" --version ^| findstr /C:"cmake version"') do set CMAKE_VER=%%G
+for /F "tokens=1 delims=." %%G in ("%CMAKE_VER%") do set CMAKE_VN_MAJOR=%%G
+echo We are using CMake version: %CMAKE_VN_MAJOR%
+
