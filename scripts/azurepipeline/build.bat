7123bd2aef26a224e1675a8b591214a39007825a
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2018-10-28 13:37:30 +0100

    More testing

--- scripts/azurepipeline/build.bat
+++ scripts/azurepipeline/build.bat
@@ -1,15 +1,15 @@
 :: Setting up dependencies
 cd ..
 git lfs install
-git clone --branch master https://github.com/LuxCoreRender/WindowsCompile .\WindowsCompile
-git clone --branch master https://github.com/LuxCoreRender/WindowsCompileDeps .\WindowsCompileDeps
+git clone --branch master https://github.com/acasta69/WindowsCompile .\WindowsCompile
+::git clone --branch master https://github.com/acasta69/WindowsCompileDeps .\WindowsCompileDeps
 
 mklink /J Luxcore .\s
 
-pip install --upgrade setuptools
-pip install --upgrade pywin32
-pip install pyinstaller
-pip install numpy==1.12.1
+::pip install --upgrade setuptools
+::pip install --upgrade pywin32
+::pip install pyinstaller
+::pip install numpy==1.12.1
 ::.\WindowsCompile\support\bin\wget.exe --user-agent="Mozilla/5.0" https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/PySide-1.2.4-cp35-cp35m-win_amd64.whl -O PySide-1.2.4-cp35-cp35m-win_amd64.whl
 ::pip install PySide-1.2.4-cp35-cp35m-win_amd64.whl
 
